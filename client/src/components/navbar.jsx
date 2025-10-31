dd31599a3a56476fb3b6f401a11fb9368c9d7b46
Danilo Gregorio <Dgg.trab@gmail.com>
2025-10-31 14:07:39 -0300

    created a simple css for login,home and navbar components

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -5,8 +5,13 @@ function Navbar(){
 
     return(
         <div className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/login">login</Link>
+            <div className="navbarcontent">
+                <h1 className="title">App</h1>
+                <div className="navbarlinks">
+                <Link to="/">Home</Link>
+                <Link to="/login">login</Link>
+                </div>
+            </div>
         </div>
     )
 
--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -4,11 +4,13 @@ function Login(){
     return(
         <div>
             <Navbar/>
+            <div className="login">
             <h1>Login</h1>
             <form>
                 <input type="text" placeholder="username" />
                 <input type="password" placeholder="password" />
             </form>
+            </div>
         </div>
     )
 }
